b21da4dca66f7feb585f3c66ee03a22769fcabc3
Vladimir Drozdik <vladimir.drozdik@gmail.com>
2022-10-18 22:15:04 +0300

    Initial commit

---
+++ src/main/java/com/tex/contextcache/TexSpringContextCache2Application.java
@@ -0,0 +1,13 @@
+package com.tex.contextcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexSpringContextCache2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TexSpringContextCache2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tex/contextcache/TexSpringContextCache2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tex.contextcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexSpringContextCache2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
